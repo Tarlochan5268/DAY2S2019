c0f23cdecd68bd03fbc564b8d498529e2beb6839
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-05-23 18:58:39 -0400

    [Tarlochan] Display my name before the Hello Java message

--- DAY2S2019/src/com/lambton/CollegeSystem.java
+++ DAY2S2019/src/com/lambton/CollegeSystem.java
@@ -4,6 +4,7 @@ public class CollegeSystem
 {
     public static void main(String[] args)
     {
+        System.out.println("This is Tarlochan");
         System.out.println("Hello Java");
     }
 }
