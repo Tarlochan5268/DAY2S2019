269dc3bd287f238312230954bd83c049558477fe
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-05-23 19:05:44 -0400

    [Tarlochan] Display my name message 10 times using FOR Loop

--- DAY2S2019/src/com/lambton/CollegeSystem.java
+++ DAY2S2019/src/com/lambton/CollegeSystem.java
@@ -5,5 +5,10 @@ public class CollegeSystem
     public static void main(String[] args)
     {
         System.out.println("Hello Java");
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i+". My name is Tarlochan");
+        }
     }
 }
