63a036d5ada515e9d4602f1d32a7ea476ae8eead
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-05-23 18:59:30 -0400

    [Tarlochan] Deleted the Display my name message

--- DAY2S2019/src/com/lambton/CollegeSystem.java
+++ DAY2S2019/src/com/lambton/CollegeSystem.java
@@ -4,7 +4,6 @@ public class CollegeSystem
 {
     public static void main(String[] args)
     {
-        System.out.println("This is Tarlochan");
         System.out.println("Hello Java");
     }
 }
